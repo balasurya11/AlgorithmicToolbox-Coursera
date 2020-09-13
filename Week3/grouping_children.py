8aec91b5b09cfc6eebd1e25fad99ddc2aa9ed24f
Balasurya M <64278574+balasurya11@users.noreply.github.com>
2020-09-13 22:44:17 +0530

    Update grouping_children.py

--- Week3/grouping_children.py
+++ Week3/grouping_children.py
@@ -1,6 +1,14 @@
+#To group children based on their age, so that the age of children in that group differs by atmost one!
+#Using Grredy Method
 def group(c):
+    
+    #Sort the age of childrem
     c.sort()
     n = len(c)
+    
+    #First step is to make a greedy move that is safe
+    #So, we have the index i coinciding with leftmost age
+    #We group the ages within the range of 1 from the first age
     val = c[0]
     i = 0
     ans = []
@@ -15,5 +23,11 @@ def group(c):
                 i = i + 1
             else:
                 break
+         
         ans.append(temp)
+        #We, then reduced it to a subproblem
+
     return ans
+
+c = list(map(int,input().split()))
+print(group(c))
