8445b0f41b2c8761a00654747eacd8fd61ce851b
balasurya11 <64278574+balasurya11@users.noreply.github.com>
2020-09-13 10:15:11 +0530

    Add files via upload

---
+++ fibonacci_huge.py
@@ -0,0 +1,16 @@
+# Uses python3
+def get_fibonacci_huge(n, m):
+	
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+
+    for _ in range(n - 1):
+        previous, current = current, (previous + current)%m
+
+    return current % m
+
+n, m = map(int, input().split())
+print(get_fibonacci_huge(n, m))
